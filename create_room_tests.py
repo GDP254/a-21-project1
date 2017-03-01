ac876319611604b622e867170ac718906edece58
GDP254 <stvnkrs@gmail.com>
2017-03-01 04:04:08 +0300

    [Chore] Add amalgamation of unit tests for create room

---
+++ create_room_tests.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from unittest import TestCase, main
+from office_tests import TestOffice
+from livingspace_tests import TestLivingSpace
+
+class TestCreateRoom(TestCase):
+	def test_create_room_successfully(self):
+		TestOffice.test_save_state_new_room()
+		TestLivingSpace.test_save_state_new_room()
+	
+if __name__ == '__main__':
+    main()
