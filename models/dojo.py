138b301ffaae4cacfd6dd3e0bb3b49ff703a8530
GDP254 <stvnkrs@gmail.com>
2017-03-03 00:15:13 +0300

    [Chore] Create stubs for dojo module

---
+++ models/dojo.py
@@ -0,0 +1,33 @@
+"""Narrative
+
+The Dojo is a facility that holds a set of uniquely named rooms.
+It should not be instantiated and should allow for the addition and removal of rooms.
+"""
+
+class Dojo(object):
+
+	name = 'Dojo'
+	__rooms = set([])
+
+	def __init__(self):
+		pass
+
+	@property
+	def room_count(cls):
+		pass
+
+	@property
+	def rooms(cls):
+		pass
+
+	@classmethod
+	def add_room(cls, room):
+		pass
+
+	@classmethod
+	def remove_room(cls, room):
+		pass
+
+	@classmethod
+	def has_room(cls, room):
+		pass
