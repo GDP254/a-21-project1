a0f4fb5c7b63e45066a1396e480de3325f7639fd
GDP254 <stvnkrs@gmail.com>
2017-03-03 07:05:29 +0300

    [Chore] Add tests for adding existing rooms

--- tests/dojo_tests.py
+++ tests/dojo_tests.py
@@ -15,7 +15,7 @@ class TestDojo(TestCase):
 		y = Dojo.has_room(room)
 		self.assertEquals([x, y], [False, True])
 
-	#Dojo should be able to add rooms
+	#Dojo should be able to add new rooms
 	def test_add_new_room(self):
 		room = Room("x")
 		initial_room_count = Dojo.room_count
@@ -25,6 +25,19 @@ class TestDojo(TestCase):
 		new_found_state = Dojo.find_room(room)
 		self.assertEquals([new_room_count, new_found_state],
 							[initial_room_count + 1, not initial_found_state])
+
+	#Dojo should not be able to add existing rooms
+	def test_add_existing_room(self):
+		room = Room("x")
+		initial_room_count = Dojo.room_count
+		initial_found_state = Dojo.has_room(room)
+		Dojo.add_room(room)
+		with self.assertRaises(ValueError):
+			Dojo.add_room(room)
+		new_room_count = Dojo.room_count()
+		new_found_state = Dojo.find_room("x")
+		self.assertEquals([new_room_count, new_found_state],
+							[initial_room_count, initial_found_state])
 
 	#Dojo should be able to remove rooms
 	def test_remove_existing_room(self):
