4174fd5e0f1a9a41644f67fd446fe1f8d6cdcad3
GDP254 <stvnkrs@gmail.com>
2017-03-06 11:32:00 +0300

    [Chore] Create interface with Docopt

---
+++ controller.py
@@ -0,0 +1,3 @@
+
+def create_room(room_name, room_type):
+	print("Room added")
--- run.py
+++ run.py
@@ -1,9 +1,25 @@
-from unittest import main
+"""Occupancy Manager
 
-from tests.dojo_tests import TestDojo
-from tests.livingspace_tests import TestLivingSpace
-from tests.office_tests import TestOffice
-from tests.fellow_tests import TestFellow
+Usage:
+  run.py create_room (<room_type> <room_name>)...
+  run.py add_person (<person_first_name> <person_last_name> <person_type>) [wants_accommodation]
+  run.py (-h | --help)
+
+Options:
+  -h, --help
+
+"""
+
+from docopt import docopt
+
+from controller import create_room
 
 if __name__ == '__main__':
-    main()
+    arguments = docopt(__doc__)
+    print(arguments)
+    if arguments['create_room']:
+    	create_room(arguments['<room_name>'], arguments['<room_type>'])
+    elif arguments['add_person']:
+    	pass
+    else:
+    	pass
