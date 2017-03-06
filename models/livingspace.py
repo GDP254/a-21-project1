b7d9383bb4fdb89cddcd264b9374976ce5c8a6ac
GDP254 <stvnkrs@gmail.com>
2017-03-06 23:24:59 +0300

    [Chore] Remove uncessecary comments

--- models/livingspace.py
+++ models/livingspace.py
@@ -8,7 +8,7 @@ class LivingSpace(Room):
 	type_ = "LivingSpace"
 	__livingspaces = dict()
 
-	@staticmethod
+	@classmethod
 	def add(cls, livingspace):
 		cls.filter_livingspace(livingspace)
 		Dojo.add_room(livingspace)
@@ -16,35 +16,21 @@ class LivingSpace(Room):
 									[{capacity: livingspace.capacity}, 
 										{type_: livingspace.type_}]})
 
-	@staticmethod
+	@classmethod
 	def remove(cls, livingspace):
 		cls.filter_livingspace(livingspace)
 		Dojo.remove_room(livingspace)
 		del cls.__livingspaces[livingspace.name]
 
-	@staticmethod
+	@classmethod
+	def rooms(cls):
+		return cls.__livingspaces
+
+	@classmethod
 	def filter_livingspace(cls, livingspace):
 		if not isinstance(livingspace, LivingSpace):
 			raise TypeError("Only type living space allowed")
 
-	"""
-	# Overide Room class, only accept instances of Fellow
-	def allocate_to(self, fellow):
-		self.filter(fellow)
-		super().allocate_to(fellow)
-	"""
-
-	"""
-	def arrogate_from(self, fellow):
-		self.filter(fellow)
-		super().arrogate_from(fellow)
-	"""
-
-	"""
-	def has_allocation(self, fellow):
-		self.filter(fellow)
-		super().has_allocation(fellow)
-	"""
 	#Only accept instances of Fellow
 	def filter(self, fellow):
 		if not isinstance(fellow, Fellow):
