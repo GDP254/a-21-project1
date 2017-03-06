5d60dae2663d76b4c5c194ae3edf838fa54abc1e
GDP254 <stvnkrs@gmail.com>
2017-03-06 22:51:15 +0300

    [Functionality] Implement docopt interface

--- controller.py
+++ controller.py
@@ -1,3 +1,34 @@
+from models.person import Person
+from models.fellow import Fellow
+from models.staff import Staff
+from models.room import Room
+from models.office import Office
+from models.livingspace import LivingSpace
+from models.dojo import Dojo
+from models.state import persons_phone, persons_detail
 
 def create_room(room_name, room_type):
-	print("Room added")
+	room_input_index = 0
+	while room_input_index < len(room_name):
+		room_type_curr = room_type[room_input_index].upper()
+		room_name_curr = room_name[room_input_index].upper()
+		room = Room(room_name_curr)
+		Dojo.add_room(room)
+		room_input_index += 1
+	print("Room Added")
+	print(Dojo.rooms())
+
+def add_person(first_name, last_name, phone, type_, opt_in="N"):
+	try:
+		type_ = type_.upper()
+		print(type_)
+		if type_ == "FELLOW":
+			fellow = Fellow(first_name, last_name, phone, opt_in)
+			fellow.register()
+		elif type_ == "STAFF":
+			staff = Staff(first_name, last_name, phone, opt_in)
+			staff.register()
+		print("Person added")
+		print(persons_detail)
+	except Exception as e:
+		print(str(e))
--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 
 Usage:
   run.py create_room (<room_type> <room_name>)...
-  run.py add_person (<person_first_name> <person_last_name> <person_type>) [wants_accommodation]
+  run.py add_person (<person_first_name> <person_last_name> <person_phone> <person_type>) [<wants_accommodation>]
   run.py (-h | --help)
 
 Options:
@@ -12,14 +12,17 @@ Options:
 
 from docopt import docopt
 
-from controller import create_room
+from controller import create_room, add_person
 
 if __name__ == '__main__':
     arguments = docopt(__doc__)
-    print(arguments)
     if arguments['create_room']:
     	create_room(arguments['<room_name>'], arguments['<room_type>'])
     elif arguments['add_person']:
-    	pass
+    	add_person(arguments['<person_first_name>'], 
+                  arguments['<person_last_name>'], 
+                  arguments['<person_phone>'], 
+                  arguments['<person_type>'], 
+                  arguments['<wants_accommodation>'])
     else:
     	pass
