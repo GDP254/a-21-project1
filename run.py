f250e25e8442e273d1a418b35507e8a04eb4f45b
GDP254 <stvnkrs@gmail.com>
2017-03-08 21:00:45 +0300

    [Functionality] Make command line interactive

--- run.py
+++ run.py
@@ -1,28 +1,85 @@
 """Occupancy Manager
 
 Usage:
-  run.py create_room (<room_type> <room_name>)...
-  run.py add_person (<person_first_name> <person_last_name> <person_phone> <person_type>) [<wants_accommodation>]
-  run.py (-h | --help)
+  run2.py create_room (<room_type> <room_name>)...
+  run2.py add_person (<person_first_name> <person_last_name> <person_phone> <person_type>) [<wants_accommodation>]
+  run2.py (-i | --interactive)
+  run2.py (-h | --help)
 
 Options:
-  -h, --help
+    -i, --interactive  Interactive Mode
+    -h, --help  Show this screen and exit.
 
 """
 
-from docopt import docopt
+import sys
+import cmd
+from docopt import docopt, DocoptExit
 
 from controller import create_room, add_person
 
-if __name__ == '__main__':
-    arguments = docopt(__doc__)
-    if arguments['create_room']:
-    	create_room(arguments['<room_name>'], arguments['<room_type>'])
-    elif arguments['add_person']:
-    	add_person(arguments['<person_first_name>'], 
+def docopt_cmd(func):
+    """
+    This decorator is used to simplify the try/except block and pass the result
+    of the docopt parsing to the called action.
+    """
+    def fn(self, arg):
+        try:
+            opt = docopt(fn.__doc__, arg)
+
+        except DocoptExit as e:
+            # The DocoptExit is thrown when the args do not match.
+            # We print a message to the user and the usage block.
+
+            print('Invalid Command!')
+            print(e)
+            return
+
+        except SystemExit:
+            # The SystemExit exception prints the usage for --help
+            # We do not need to do the print here.
+
+            return
+
+        return func(self, opt)
+
+    fn.__name__ = func.__name__
+    fn.__doc__ = func.__doc__
+    fn.__dict__.update(func.__dict__)
+    return fn
+
+
+class MyInteractive (cmd.Cmd):
+    intro = 'Welcome to Dojo Occupancy Manager!' \
+        + ' (type help for a list of commands.)'
+    prompt = '(run2.py) '
+    file = None
+
+    @docopt_cmd
+    def do_create_room(self, arg):
+        """Usage: create_room (<room_type> <room_name>)..."""
+        print(arg)
+        create_room(arg['<room_name>'], arg['<room_type>'])
+
+    @docopt_cmd
+    def do_add_person(self, arg):
+        """Usage: add_person (<person_first_name> <person_last_name> <person_phone> <person_type>) [<wants_accommodation>]"""
+        print(arg)
+        add_person(arguments['<person_first_name>'], 
                   arguments['<person_last_name>'], 
                   arguments['<person_phone>'], 
                   arguments['<person_type>'], 
                   arguments['<wants_accommodation>'])
-    else:
-    	pass
+
+    def do_quit(self, arg):
+        """Quits out of Interactive Mode."""
+
+        print('Good Bye!')
+        exit()
+
+opt = docopt(__doc__, sys.argv[1:])
+
+if opt['--interactive']:
+    MyInteractive().cmdloop()
+
+print(opt)
