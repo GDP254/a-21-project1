e922e55e00ec589f1e643ac3db56f7a7e6f45c84
GDP254 <stvnkrs@gmail.com>
2017-03-09 07:40:46 +0300

    [Chore] Add unit tests for print functionality

--- tests.py
+++ tests.py
@@ -4,6 +4,7 @@ from tests.dojo_tests import TestDojo
 from tests.livingspace_tests import TestLivingSpace
 from tests.office_tests import TestOffice
 from tests.fellow_tests import TestFellow
+from tests.print_tests import TestPrint
 
 if __name__ == '__main__':
     main()
---
+++ tests/print_tests.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from unittest import TestCase, main
+
+from models.dojo import Dojo
+from models.room import Room
+from models.office import Office
+from models.livingspace import LivingSpace
+from models.fellow import Fellow
+from models.staff import Staff
+from models.person import Person
+
+class TestPrint(TestCase):
+
+	def test_print_populated_room(self):
+		self.clear_stores()
+		office = Office("NDO")
+		Dojo.add_room(office)
+		fellow = Fellow("Xone", "Ndobile", "0856443334", "y")
+		fellow.register()
+		office.allocate_to(fellow)
+		allocations = office.allocations()
+		output = Room.members(allocations)
+		self.assertEquals(output, "0856443334, NDOBILE, XONE, FELLOW")
+
+	def test_print_empty_room(self):
+		self.clear_stores()
+		livingspace = LivingSpace("NDO1")
+		output = livingspace.allocations()
+		self.assertEquals(output, "%s currently has no members." % livingspace.name)
+
+	def test_print_existing_allocations_to_screen(self):
+		self.clear_stores()
+		office = Office("NDO2")
+		Dojo.add_room(office)
+		fellow = Fellow("Xone2", "Ndobile2", "0856443324", "y")
+		fellow.register()
+		office.allocate_to(fellow)
+		allocations = Room.all_allocations()
+		output = Room.members(allocations)
+		expected_output = "ND02, 0856443324, NDOBILE2, XONE2, FELLOW"
+		self.assertEquals(output, expected_output)
+
+	def test_print_existing_unallocated_to_screen(self):
+		self.clear_stores()
+		office = Office("NDO4")
+		Dojo.add_room(office)
+		fellow = Fellow("Xone4", "Ndobile4", "0856443354", "y")
+		fellow.register()
+		office.allocate_to(fellow)
+		staff = Staff("Xone3", "Ndobile3", "0856443344", "y")
+		staff.register()
+		unallocated = Room.all_unallocated()
+		output = Room.members(unallocated)
+		expected_output = "0856443344, NDOBILE3, XONE3, STAFF"
+		self.assertEquals(output, expected_output)
+
+	def clear_stores(self):
+		#Clean data stores to run print tests
+		Dojo.clear()
+		Room.clear()
+		Person.clear()
+
+if __name__ == '__main__':
+    main()
