62a9bd7594c9f8cff0559ec9d1fcd1c9ae196472
GDP254 <stvnkrs@gmail.com>
2017-03-12 11:04:27 +0300

    [Chore] Add test for output to a specific file

--- tests/print_tests.py
+++ tests/print_tests.py
@@ -41,7 +41,7 @@ class TestPrint(TestCase):
 		expected_output = "NDO2-Office, 0856443324, NDOBILE2, XONE2, FELLOW\n"
 		self.assertEqual(output, expected_output)
 
-	def test_print_existing_allocations_to_file(self):
+	def test_print_existing_allocations_to_default_file(self):
 		self.clear_stores()
 		office = Office("ND2")
 		Dojo.add_room(office)
@@ -51,7 +51,25 @@ class TestPrint(TestCase):
 		allocations_ = Room.all_allocations()
 		expected_output = Room.members(allocations_, room_tag=True)
 		Room.to_file(expected_output)
-		f = open("file.txt", "r")
+		path = "output/"
+		f = open(path+"File.txt", "r")
+		output = f.read() #"NDO2-Office, 0856443324, NDOBILE2, XONE2, FELLOW"
+		f.close()
+		self.assertEqual(expected_output, output)
+
+	def test_print_existing_allocations_to_specific_file(self):
+		self.clear_stores()
+		office = Office("ND88")
+		Dojo.add_room(office)
+		fellow = Fellow("Xne88", "Ndoile88", "086800000", "y")
+		fellow.register()
+		office.allocate_to(fellow)
+		allocations_ = Room.all_allocations()
+		expected_output = Room.members(allocations_, room_tag=True)
+		file_name = office.name+"-Allocations"
+		Room.to_file(expected_output, file_name)
+		path = "output/"
+		f = open(path+file_name+".txt", "r")
 		output = f.read() #"NDO2-Office, 0856443324, NDOBILE2, XONE2, FELLOW"
 		f.close()
 		self.assertEqual(expected_output, output)
